10529426ca7f696bbd8583fd3e8f57bacfdd655c
Dominique Gajardo <dominique.gajardoap@gmail.com>
2024-03-30 18:09:36 -0300

    Cambio de padding en formulario

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -2,7 +2,7 @@ import SocialButton from "./SocialButton" //importamos componente
 import Formulario from "./Formulario" //importamos componente
 const Registro = () => { //funcion que nos renderizara los componentes en un solo componente
     return ( //la funcions nos devuelve los componentes mas datos agregados 
-        <div className="registro pt-2 pb-2 mt-2 "> 
+        <div className="registro mt-2 "> 
             <h1>Crea tu cuenta</h1>
             <SocialButton />
             <Formulario 
